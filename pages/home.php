73c573fbb0652e232dbb61152057e99d85b38f4e
MarioWindsor <mario@lazaro.in>
2020-07-17 13:40:28 +0530

    Build Part 2

--- pages/home.php
+++ pages/home.php
@@ -54,38 +54,74 @@
 <section class="intro-section fill-neutral-1 space-150-top-bottom" data-section-title="Intro Section" data-section-slug="intro-section">
 	<div class="row space-50-bottom">
 		<div class="container">
-			<div class="columns small-12 h3">
-				We guide students, corporate executives, faculty & professionals on <span class="strong">study-centric, experiential learning programs in urban and rural India.</span>
+			<div class="columns small-12 medium-10 medium-offset-1 large-8">
+				<div class="h3">
+					We guide students, corporate executives, faculty and professionals on <span class="strong"><span class="no-wrap">study-centric</span>, experiential learning programs in urban and rural India.</span>
+				</div>
 			</div>
 		</div>
 	</div>
 	<div class="row fill-neutral-2 space-50-top-bottom">
 		<div class="container">
-			<div class="columns small-12">
-				New
-				Virtual Learning Series
-				<!-- insert video -->
-				90 minute live virtual sessions:
-				- Business Case Studies
-				- Business Leader Profiles
-				- Problem Solving Workshops
-				- End with a Q&A Session
-				- Customisable on Request
-				<button>Register Now</button>
+			<div class="columns small-12 medium-10 medium-offset-1 large-10">
+				<div class="h4 strong text-teal text-uppercase">New</div>
+				<div class="h4 strong space-min-bottom">Virtual Learning Series</div>
+				<div class="row">
+					<div class="columns small-12 large-7 space-min-bottom">
+						<!-- video embed -->
+						<div class="video-embed js_video_embed" data-src="lncVHzsc_QA">
+							<div class="video-loading-indicator"></div>
+						</div>
+					</div>
+					<div class="columns small-12 large-5 space-25-left">
+						<div class="space-25-bottom">
+							<div class="h5 strong space-25-bottom">90 minute Live Virtual Sessions:</div>
+							<div class="h5"><span class="strong text-teal">— &nbsp;</span> Business Case Studies</div>
+							<div class="h5"><span class="strong text-teal">— &nbsp;</span> Business Leader Profiles</div>
+							<div class="h5"><span class="strong text-teal">— &nbsp;</span> Problem Solving Workshops</div>
+							<div class="h5"><span class="strong text-teal">— &nbsp;</span> End with a Q&A Session</div>
+							<div class="h5"><span class="strong text-teal">— &nbsp;</span> Customisable on Request</div>
+						</div>
+						<button class="fill-teal">Register Now</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 	<div class="row space-50-top">
 		<div class="container">
-			<div class="columns small-12">
-				Travel. Experience. Learn. Repeat.
-				What you can learn from first-hand experiences in a culturally-diverse developing nation like India, is so much more than what textbooks can teach you. We’ve got learning experiences for everyone – students, corporates, faculty & professionals!
-				Customised High-Impact Experiences
-				What you want is what you get! You could begin your journey at a start-up company in an Urban Metropolis and maybe wind up in a Wildlife Sanctuary – with us, anything’s possible! Our programs are designed to pack a wide variety of experiences into a 10-15 day schedule
-				Researched & Handpicked Programs
-				We have dedicated academic and cultural experts skilled in researching trends and current issues that can provide high-impact learning experiences. We work closely with faculty and program leaders, to get more clarity on how best to adapt study experiences to complement specific academic requirements.
-				We’ve Got It Covered
-				Planning the A to Z of your study experience in a foreign country is quite challenging; that requires the ‘We’ve Got It Covered’ superpower - be it airport transfers, local transport, hotel accommodation, breakfast, lunch, dinner, ferry tickets or travel insurance, we’ve got you covered.
+			<div class="columns small-12 medium-10 medium-offset-1 large-8">
+				<div class="h4 strong text-pink space-min-bottom">Travel. Experience. Learn. Repeat.</div>
+				<div class="h5 space-50-bottom">What you can learn from first-hand experiences in a culturally-diverse developing nation like India, is so much more than what textbooks can teach you. We’ve got learning experiences for everyone – students, corporates, faculty & professionals!</div>
+			</div>
+			<div class="columns small-12 medium-10 medium-offset-1 large-6">
+				<div class="point">
+					<div class="h4 text-pink space-min-bottom">Customised High-Impact Experiences</div>
+					<div class="row">
+						<div class="underline columns small-4 medium-3 space-min-bottom"><span class="fill-pink"></span></div>
+						<div class="columns small-12 medium-9">
+							<div class="p text-neutral-4 space-50-bottom">What you want is what you get! You could begin your journey at a start-up company in an Urban Metropolis and maybe wind up in a Wildlife Sanctuary – with us, anything’s possible! Our programs are designed to pack a wide variety of experiences into a 10-15 day schedule</div>
+						</div>
+					</div>
+				</div>
+				<div class="point">
+					<div class="h4 text-pink space-min-bottom">Researched & Handpicked Programs</div>
+					<div class="row">
+						<div class="underline columns small-4 medium-3 space-min-bottom"><span class="fill-pink"></span></div>
+						<div class="columns small-12 medium-9">
+							<div class="p text-neutral-4 space-50-bottom">We have dedicated academic and cultural experts skilled in researching trends and current issues that can provide high-impact learning experiences. We work closely with faculty and program leaders, to get more clarity on how best to adapt study experiences to complement specific academic requirements.</div>
+						</div>
+					</div>
+				</div>
+				<div class="point">
+					<div class="h4 text-pink space-min-bottom">We’ve Got It Covered</div>
+					<div class="row">
+						<div class="underline columns small-4 medium-3 space-min-bottom"><span class="fill-pink"></span></div>
+						<div class="columns small-12 medium-9">
+							<div class="p text-neutral-4 space-50-bottom">Planning the A to Z of your study experience in a foreign country is quite challenging; that requires the ‘We’ve Got It Covered’ superpower - be it airport transfers, local transport, hotel accommodation, breakfast, lunch, dinner, ferry tickets or travel insurance, we’ve got you covered.</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -105,11 +141,11 @@
 <!-- END: Gallery Section -->
 
 <!-- Quote Section -->
-<section class="quote-section" data-section-title="Quote Section Virtual" data-section-slug="quote-section-virtual">
+<section class="quote-section space-150-top-bottom fill-pink" data-section-title="Quote Section Travel" data-section-slug="quote-section-travel">
 	<div class="container">
 		<div class="row">
-			<div class="columns small-12">
-				
+			<div class="columns small-12 medium-10 medium-offset-1 xlarge-8 xlarge-offset-2">
+				<div class="h3 text-center"><span class="cursive">“</span> When overseas you learn more about your own country, than you do the place you’re visiting. <span class="cursive">”</span> – <span class="cursive">Clint Borgen.</span></div>
 			</div>
 		</div>
 	</div>
@@ -153,11 +189,11 @@
 <!-- END: Tours Section -->
 
 <!-- Quote Section -->
-<section class="quote-section" data-section-title="Quote Section Travel" data-section-slug="quote-section-travel">
+<section class="quote-section space-150-top-bottom fill-teal" data-section-title="Quote Section Virtual" data-section-slug="quote-section-virtual">
 	<div class="container">
 		<div class="row">
-			<div class="columns small-12">
-				
+			<div class="columns small-12 medium-10 medium-offset-1 xlarge-8 xlarge-offset-2">
+				<div class="h3 text-center"><span class="cursive">“</span> One’s destination is never a place, but rather a new way of looking at things. <span class="cursive">”</span> – <span class="cursive">Henry Miller.</span></div>
 			</div>
 		</div>
 	</div>
