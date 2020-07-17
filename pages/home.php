7f635d4a12821ee13e0e3aa3a2a956b1941be2ab
MarioWindsor <mario@lazaro.in>
2020-07-17 16:26:11 +0530

    Build Part 3

--- pages/home.php
+++ pages/home.php
@@ -40,6 +40,7 @@
 
 <!-- Landing Section -->
 <section class="landing-section" data-section-title="Landing Section" data-section-slug="landing-section">
+	<img class="block" src="https://via.placeholder.com/1920x1080">
 	<div class="container">
 		<div class="row">
 			<div class="columns small-12">
@@ -55,7 +56,7 @@
 	<div class="row space-50-bottom">
 		<div class="container">
 			<div class="columns small-12 medium-10 medium-offset-1 large-8">
-				<div class="h3">
+				<div class="h4 line-height-medium">
 					We guide students, corporate executives, faculty and professionals on <span class="strong"><span class="no-wrap">study-centric</span>, experiential learning programs in urban and rural India.</span>
 				</div>
 			</div>
@@ -65,7 +66,7 @@
 		<div class="container">
 			<div class="columns small-12 medium-10 medium-offset-1 large-10">
 				<div class="h4 strong text-teal text-uppercase">New</div>
-				<div class="h4 strong space-min-bottom">Virtual Learning Series</div>
+				<div class="h2 space-min-bottom">Virtual Learning Series</div>
 				<div class="row">
 					<div class="columns small-12 large-7 space-min-bottom">
 						<!-- video embed -->
@@ -75,12 +76,12 @@
 					</div>
 					<div class="columns small-12 large-5 space-25-left">
 						<div class="space-25-bottom">
-							<div class="h5 strong space-25-bottom">90 minute Live Virtual Sessions:</div>
+							<div class="h4 strong space-25-bottom">90 minute Live Virtual Sessions</div>
 							<div class="h5"><span class="strong text-teal">— &nbsp;</span> Business Case Studies</div>
 							<div class="h5"><span class="strong text-teal">— &nbsp;</span> Business Leader Profiles</div>
 							<div class="h5"><span class="strong text-teal">— &nbsp;</span> Problem Solving Workshops</div>
 							<div class="h5"><span class="strong text-teal">— &nbsp;</span> End with a Q&A Session</div>
-							<div class="h5"><span class="strong text-teal">— &nbsp;</span> Customisable on Request</div>
+							<div class="h5"><span class="strong text-teal">— &nbsp;</span> Customizable on Request</div>
 						</div>
 						<button class="fill-teal">Register Now</button>
 					</div>
@@ -91,34 +92,34 @@
 	<div class="row space-50-top">
 		<div class="container">
 			<div class="columns small-12 medium-10 medium-offset-1 large-8">
-				<div class="h4 strong text-pink space-min-bottom">Travel. Experience. Learn. Repeat.</div>
-				<div class="h5 space-50-bottom">What you can learn from first-hand experiences in a culturally-diverse developing nation like India, is so much more than what textbooks can teach you. We’ve got learning experiences for everyone – students, corporates, faculty & professionals!</div>
+				<div class="h2 text-pink space-min-bottom">Travel. Experience. Learn. Repeat.</div>
+				<div class="h5">What you can learn from first-hand experiences in a culturally-diverse developing nation like India, is so much more than what textbooks can teach you. We’ve got learning experiences for everyone – students, corporates, faculty & professionals!</div>
 			</div>
-			<div class="columns small-12 medium-10 medium-offset-1 large-6">
-				<div class="point">
-					<div class="h4 text-pink space-min-bottom">Customised High-Impact Experiences</div>
+			<div class="columns small-12 medium-10 medium-offset-1">
+				<div class="point space-50-top">
+					<div class="h4 text-pink space-min-bottom">Customized High-Impact Experiences</div>
 					<div class="row">
-						<div class="underline columns small-4 medium-3 space-min-bottom"><span class="fill-pink"></span></div>
-						<div class="columns small-12 medium-9">
-							<div class="p text-neutral-4 space-50-bottom">What you want is what you get! You could begin your journey at a start-up company in an Urban Metropolis and maybe wind up in a Wildlife Sanctuary – with us, anything’s possible! Our programs are designed to pack a wide variety of experiences into a 10-15 day schedule</div>
+						<div class="underline columns small-4 medium-3 large-2 space-min-bottom"><span class="fill-pink"></span></div>
+						<div class="columns small-12 medium-9 large-6 xlarge-5">
+							<div class="p text-neutral-4">What you want is what you get! You could begin your journey at a start-up company in an Urban Metropolis and maybe wind up in a Wildlife Sanctuary – with us, anything’s possible! Our programs are designed to pack a wide variety of experiences into a 10-15 day schedule</div>
 						</div>
 					</div>
 				</div>
-				<div class="point">
+				<div class="point space-50-top">
 					<div class="h4 text-pink space-min-bottom">Researched & Handpicked Programs</div>
 					<div class="row">
-						<div class="underline columns small-4 medium-3 space-min-bottom"><span class="fill-pink"></span></div>
-						<div class="columns small-12 medium-9">
-							<div class="p text-neutral-4 space-50-bottom">We have dedicated academic and cultural experts skilled in researching trends and current issues that can provide high-impact learning experiences. We work closely with faculty and program leaders, to get more clarity on how best to adapt study experiences to complement specific academic requirements.</div>
+						<div class="underline columns small-4 medium-3 large-2 space-min-bottom"><span class="fill-pink"></span></div>
+						<div class="columns small-12 medium-9 large-6 xlarge-5">
+							<div class="p text-neutral-4">We have dedicated academic and cultural experts skilled in researching trends and current issues that can provide high-impact learning experiences. We work closely with faculty and program leaders, to get more clarity on how best to adapt study experiences to complement specific academic requirements.</div>
 						</div>
 					</div>
 				</div>
-				<div class="point">
+				<div class="point space-50-top">
 					<div class="h4 text-pink space-min-bottom">We’ve Got It Covered</div>
 					<div class="row">
-						<div class="underline columns small-4 medium-3 space-min-bottom"><span class="fill-pink"></span></div>
-						<div class="columns small-12 medium-9">
-							<div class="p text-neutral-4 space-50-bottom">Planning the A to Z of your study experience in a foreign country is quite challenging; that requires the ‘We’ve Got It Covered’ superpower - be it airport transfers, local transport, hotel accommodation, breakfast, lunch, dinner, ferry tickets or travel insurance, we’ve got you covered.</div>
+						<div class="underline columns small-4 medium-3 large-2 space-min-bottom"><span class="fill-pink"></span></div>
+						<div class="columns small-12 medium-9 large-6 xlarge-5">
+							<div class="p text-neutral-4">Planning the A to Z of your study experience in a foreign country is quite challenging; that requires the ‘We’ve Got It Covered’ superpower - be it airport transfers, local transport, hotel accommodation, breakfast, lunch, dinner, ferry tickets or travel insurance, we’ve got you covered.</div>
 						</div>
 					</div>
 				</div>
@@ -130,10 +131,11 @@
 
 <!-- Gallery Section -->
 <section class="gallery-section" data-section-title="Gallery Section" data-section-slug="gallery-section">
+	<img class="block" src="https://via.placeholder.com/1920x1080">
 	<div class="container">
 		<div class="row">
 			<div class="columns small-12">
-				
+
 			</div>
 		</div>
 	</div>
@@ -141,59 +143,85 @@
 <!-- END: Gallery Section -->
 
 <!-- Quote Section -->
-<section class="quote-section space-150-top-bottom fill-pink" data-section-title="Quote Section Travel" data-section-slug="quote-section-travel">
+<section class="quote-section space-100-top-bottom fill-pink" data-section-title="Quote Section Travel" data-section-slug="quote-section-travel">
 	<div class="container">
 		<div class="row">
-			<div class="columns small-12 medium-10 medium-offset-1 xlarge-8 xlarge-offset-2">
-				<div class="h3 text-center"><span class="cursive">“</span> When overseas you learn more about your own country, than you do the place you’re visiting. <span class="cursive">”</span> – <span class="cursive">Clint Borgen.</span></div>
+			<div class="columns small-12 medium-10 medium-offset-1 large-8 large-offset-2">
+				<div class="h4 text-center line-height-medium"><span class="cursive">“</span> When overseas you learn more about your own country, than you do the place you’re visiting. <span class="cursive">”</span> – <span class="cursive">Clint Borgen.</span></div>
 			</div>
 		</div>
 	</div>
 </section>
 <!-- END: Quote Section -->
 
-<!-- Video Section -->
-<section class="video-section" data-section-title="Video Section" data-section-slug="video-section">
-	<div class="container">
-		<div class="row">
-			<div class="columns small-12">
-				
-			</div>
-		</div>
-	</div>
-</section>
-<!-- END: Video Section -->
-
 <!-- Packages Section -->
-<section class="packages-section" data-section-title="Packages Section" data-section-slug="packages-section">
+<section class="packages-section space-150-top-bottom fill-neutral-1" data-section-title="Packages Section" data-section-slug="packages-section">
 	<div class="container">
 		<div class="row">
-			<div class="columns small-12">
-				
+			<div class="columns small-12 medium-10 medium-offset-1">
+				<div class="h2 text-uppercase space-min-bottom">Immersive Study Programs</div>
+			</div>
+			<div class="columns small-12 medium-10 medium-offset-1">
+				<div class="row">
+					<div class="underline columns small-4 medium-3 large-2 space-min-bottom"><span class="fill-pink"></span></div>
+					<div class="columns small-12 medium-9 large-6 xlarge-5">
+						<div class="h4 text-pink">Travel Based</div>
+						<div class="p text-neutral-4 space-min-bottom">12 to 15 day immersive travel study programs to India. Customized itineraries with turn-key logistics.</div>
+						<div class="h4 text-teal">Virtual Series</div>
+						<div class="p text-neutral-4">90 minute, live video programs that include a Q&A session.</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </section>
 <!-- END: Packages Section -->
 
-<!-- Tours Section -->
-<section class="tours-section" data-section-title="Tours Section" data-section-slug="tours-section">
+<!-- Articles Section -->
+<section class="articles-section space-150-top-bottom fill-neutral-2" data-section-title="Articles Section" data-section-slug="articles-section">
 	<div class="container">
 		<div class="row">
-			<div class="columns small-12">
-				
+			<div class="columns small-12 medium-10 medium-offset-1">
+				<div class="h2 text-uppercase space-min-bottom">Articles</div>
+				<div class="row">
+					<div class="underline columns small-4 medium-3 large-2 space-25-bottom"><span class="fill-teal"></span></div>
+				</div>
+				<div class="p text-neutral-4">Filter By: Articles — Journals — News</div>
 			</div>
 		</div>
 	</div>
 </section>
-<!-- END: Tours Section -->
+<!-- END: Articles Section -->
 
-<!-- Quote Section -->
-<section class="quote-section space-150-top-bottom fill-teal" data-section-title="Quote Section Virtual" data-section-slug="quote-section-virtual">
+<!-- Brochure Section -->
+<section class="brochure-section space-150-top-bottom fill-dark" data-section-title="Brochure Section" data-section-slug="brochure-section">
 	<div class="container">
 		<div class="row">
-			<div class="columns small-12 medium-10 medium-offset-1 xlarge-8 xlarge-offset-2">
-				<div class="h3 text-center"><span class="cursive">“</span> One’s destination is never a place, but rather a new way of looking at things. <span class="cursive">”</span> – <span class="cursive">Henry Miller.</span></div>
+			<div class="columns small-12 medium-10 medium-offset-1 large-8">
+				<div class="h2 text-uppercase space-min-bottom">In a hurry</div>
+				<div class="row">
+					<div class="underline columns small-4 medium-3 large-2 space-25-bottom"><span class="fill-orange"></span></div>
+				</div>
+				<div class="row">
+					<div class="columns small-12 medium-6">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam pariatur quis incidunt tenetur maxime eveniet facere nesciunt ullam ex quaerat similique aspernatur sed, vero velit, suscipit laborum deserunt iste iure.
+					</div>
+					<div class="columns small-12 medium-6">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis sed delectus, eveniet, rerum necessitatibus voluptatibus ducimus ipsa autem maiores magni nihil debitis laboriosam assumenda tempore doloremque earum, error hic eius.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Brochure Section -->
+
+<!-- Quote Section -->
+<section class="quote-section space-100-top-bottom fill-teal" data-section-title="Quote Section Virtual" data-section-slug="quote-section-virtual">
+	<div class="container">
+		<div class="row">
+			<div class="columns small-12 medium-10 medium-offset-1 large-8 large-offset-2">
+				<div class="h4 text-center line-height-medium"><span class="cursive">“</span> One’s destination is never a place, but rather a new way of looking at things. <span class="cursive">”</span> – <span class="cursive">Henry Miller.</span></div>
 			</div>
 		</div>
 	</div>
@@ -201,11 +229,19 @@
 <!-- END: Quote Section -->
 
 <!-- Booking Section -->
-<section class="booking-section" data-section-title="Booking Section" data-section-slug="booking-section">
+<section class="booking-section space-150-top-bottom fill-neutral-1" data-section-title="Booking Section" data-section-slug="booking-section">
 	<div class="container">
 		<div class="row">
-			<div class="columns small-12">
-				
+			<div class="columns small-12 medium-10 medium-offset-1">
+				<div class="h2 text-uppercase space-min-bottom">Customize Your Program</div>
+				<div class="row">
+					<div class="underline columns small-4 medium-3 large-2 space-min-bottom"><span class="fill-pink"></span></div>
+					<!-- Form -->
+					<div class="form columns small-8 medium-9 large-10">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi enim facilis id molestias a omnis, eum amet, reprehenderit delectus animi libero earum cum saepe facere quidem optio unde, odit quia!
+					</div>
+					<!-- END: Form -->
+				</div>
 			</div>
 		</div>
 	</div>
@@ -213,11 +249,19 @@
 <!-- END: Booking Section -->
 
 <!-- Team Section -->
-<section class="team-section" data-section-title="Team Section" data-section-slug="team-section">
+<section class="team-section space-150-top-bottom fill-dark" data-section-title="Team Section" data-section-slug="team-section">
 	<div class="container">
 		<div class="row">
-			<div class="columns small-12">
-				
+			<div class="columns small-12 medium-10 medium-offset-1">
+				<div class="h2 text-uppercase space-min-bottom">Meet the Team</div>
+				<div class="row">
+					<div class="underline columns small-4 medium-3 large-2 space-min-bottom"><span class="fill-orange"></span></div>
+					<!-- Team -->
+					<div class="team columns small-8 medium-9 large-10">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi enim facilis id molestias a omnis, eum amet, reprehenderit delectus animi libero earum cum saepe facere quidem optio unde, odit quia!
+					</div>
+					<!-- END: Team -->
+				</div>
 			</div>
 		</div>
 	</div>
