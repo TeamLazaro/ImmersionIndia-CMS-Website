0a351597f3d91d707fc7927f86877aad742f0257
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-07-24 16:34:58 +0530

    BFS : CMS : Theme : Update this

---
+++ cms/wp-content/themes/bfs/footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Brownie Fudge Sundae
+ * @since 1.1.0
+ */
+
+wp_footer();
---
+++ cms/wp-content/themes/bfs/header.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Brownie Fudge Sundae
+ * @since 1.1.0
+ */
