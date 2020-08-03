5dc877102e84e56f6ef2d26f5fcd10cb6a2c3b84
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-08-03 15:28:47 +0530

    BFS : Utility function : onScroll : Add this one

--- js/modules/utils.js
+++ js/modules/utils.js
@@ -13,3 +13,35 @@ function waitFor ( seconds ) {
 		}, seconds * 1000 );
 	} );
 }
+
+
+/*
+ *
+ *
+ *
+ */
+function onScroll ( fn ) {
+	// Add the provided function to the queue
+	window.__BFS = window.__BFS || { };
+	var scrollHandlers = window.__BFS.scrollHandlers = window.__BFS.scrollHandlers || [ ];
+	scrollHandlers.push( fn );
+
+	// Set up the scroll event handling mechanism
+	if ( scrollHandlers.length === 1 ) {
+		var previousScrollY = 0;
+		window.addEventListener( "scroll", function ( event ) {
+			currentScrollY = window.scrollY || document.body.scrollTop;
+
+			// Call all the registered scroll handlers one after the other, providing useful data
+			var _i;
+			for ( _i = 0; _i < scrollHandlers.length; _i += 1 )
+				scrollHandlers[ _i ].call( {
+					currentScrollY: currentScrollY,
+					previousScrollY: previousScrollY
+				} );
+
+			previousScrollY = currentScrollY;
+		}, true );
+	}
+
+}
