f7f66d33bebdbc88ed2f65216f79475352d97962
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-08-03 11:23:28 +0530

    Home : Programs : Documentation : Add a bit

--- js/programs.js
+++ js/programs.js
@@ -1,6 +1,15 @@
 
+/*
+ *
+ * ----- Programs
+ *
+ */
+
 $( function () {
 
+	/*
+	 * ----- Filter the programs on interacting with the filter
+	 */
 	var $allPrograms = $( ".js_program" );
 	$( document ).on( "change", ".js_program_filter", function ( event ) {
 		var programType = $( event.target ).val()
