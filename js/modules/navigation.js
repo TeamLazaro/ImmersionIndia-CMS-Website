d6dbe618b1d8b0dbb5de08e6a56ed2df27bf75b1
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-08-07 14:49:49 +0530

    BFS : Navigation : Smooth Scrolling to Sections : Add this feature

--- js/modules/navigation.js
+++ js/modules/navigation.js
@@ -65,6 +65,48 @@ onScroll( showOrHideNavigationMenuButton );
 
 
 
+/*
+ *
+ * ----- Smooth-scroll to sections
+ *
+ */
+$( document ).on( "click", "a[ href ]", function ( event ) {
+
+	var $anchor = $( event.target ).closest( "a" );
+	var domAnchor = $anchor.get( 0 );
+
+	var urlParts = domAnchor.href.split( "#" );
+	// If the url has more than one `#`es in it, we're not even going to try
+	if ( urlParts.length !== 2 )
+		return;
+
+	var path = urlParts[ 0 ];
+	var sectionId = urlParts[ 1 ];
+
+	// If the path does not match that of the current page
+	if ( path !== window.location.href )
+		return;
+
+	// If the section id is empty or a stub
+	if ( ! sectionId.trim() || sectionId === "0" )
+		return;
+
+	// Prevent default behaviour
+	event.preventDefault();
+	event.stopPropagation();
+	event.stopImmediatePropagation();
+
+	// Close the navigation menu
+	closeNavigationMenu();
+
+	smoothScrollTo( sectionId );
+
+	return false;
+
+} );
+
+
+
 
 
 
